e11fe8c6e937126a62531a8deed71a1a387634fa
Nick Clark <nicholas.clark@gmail.com>
2017-11-02 09:29:51 -0600

    In-progress commit of first filesystem

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+import sys
 import bridge
-from bridge import FuseBridge, load_string_array, Errno, Fcntl, Stat
-
-hello_str = "Hello World!\n";
-hello_path = "/hello";
+from bridge import FuseBridge, Errno, Fcntl, Stat
+from bridge import create_char_array, load_char_ptr
 
 
-def hello_open(path, info)
+hello_str = "Hello World!\n"
+hello_path = "/hello"
+
+
+def hello_open(path, info):
     assert isinstance(info, bridge.FileInfo)
     assert isinstance(path, str)
 
@@ -26,7 +29,7 @@ def hello_readdir(path, target, allocator):
     assert isinstance(allocator, bridge.AllocPtrType)
 
     results = [".", "..", hello_path[1:], "moto"]
-    load_string_array(results, target, allocator)
+    target[0] = create_char_array(results, target, allocator)
     return 0
 
 
@@ -52,9 +55,43 @@ def hello_getattr(path, attributes):
     return 0
 
 
+def hello_read(path, target, size, offset, info):
+    assert isinstance(info, bridge.FileInfo)
+    assert isinstance(target, bridge.char_ptr)
+    assert isinstance(allocator, bridge.AllocPtrType)
+
+    length = len(hello_str)
+
+    if path != hello_path:
+        return -Errno.ENOENT
+
+    if offset >= length:
+        return 0
+
+    size = min(size, length - offset)
+    load_char_ptr(hello_str[offset:offset + size], target)
+
+    return size
+
+
+def hello_main(argv):
+    assert isinstance(argv, (list, tuple))
+
+    fuse = FuseBridge()
+    fuse.callbacks.open = fuse.types.OpenPtrType(hello_open)
+    fuse.callbacks.readdir = fuse.types.ReadDirPtrType(hello_readdir)
+    fuse.callbacks.getattr = fuse.types.GetAttrPtrType(hello_getattr)
+    fuse.callbacks.read = fuse.types.ReadPtrType(hello_read)
+    fuse.callbacks.write = fuse.types.WritePtrType(hello_write)
+
+    argc = len(argv)
+    argv_pointer = create_char_array(argv)
+
+    return fuse.bridge_main(argc, argv_pointer)
+
+
 def main():
-    bridge = FuseBridge()
+    hello_main(sys.argv)
 
 if __name__ == "__main__":
-    main()s
-
+    main()
